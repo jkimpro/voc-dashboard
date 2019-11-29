a583809b4d61e00f57ca6105cdd5f0b197d5c958
jkimpor <jkimpro10@gmail.com>
2019-11-29 18:02:27 +0900

    ui prototype complete

--- public/javascripts/dropdown.js
+++ public/javascripts/dropdown.js
@@ -2,10 +2,9 @@ let dateConditionArr = ['접수일', '주문일'];
 let srConditionArr = ['SR대분류', 'SR중분류', 'SR소분류'];
 let codeArr = ['상품코드', '접수코드', '주문코드', '상품명'];
 
-
 let dropDownChange = (option, num) =>{
     if(option ===0){
-        let origin = document.getElementById('dateConditionDropDown');
+        let origin = document.getElementById('dateConditionDropdown');
         origin.innerText ="";
         origin.innerText = dateConditionArr[num];
     }
--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,31 +1,61 @@
-let colCount =0;
 
-let getData = () =>{
+let colCount =0;
+let getDate = ()=>{
+
+    let today = new Date();   
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+    
+    let result = String(year) + String(month) + String(date)
+
+    return result;
+}
+let dateRange = [getDate(),getDate()];
+
+
+$(function() {
+    $('input[name="dates"]').daterangepicker({
+        opens: 'left',
+        locale:{
+        format:'YYYY/MM/DD'
+        }
+    }, function(start, end, label) {
+        dateRange[0] = start.format('YYYYMMDD');
+        dateRange[1] = end.format('YYYYMMDD');
+        console.log('setting the date' + start.format('YYYYMMDD') + 'to' + end.format('YYYYMMDD'));
+    });
+});
+
+let getData = (dateConditionDropdown,dates,conditionDropdown,prdCondition,codeDropdown,code) =>{
 
 }
 
+let setTableData = (data) =>{
+
+}
 let serachData = () =>{
 
-    let dateConditionDropDown = document.getElementById('dateConditionDropDown').innerText;
-    let dates =  document.getElementById('dates').innerHTML;
-    
+    let dateConditionDropdown = document.getElementById('dateConditionDropdown').innerText;
+    let dates = dateRange;
+
     let conditionDropdown = document.getElementById('conditionDropdown').innerText;
-    let prdCondition = document.getElementById('prdCondition').innerText;
+    let prdCondition = document.getElementById('prdCondition').value;
     
     let codeDropdown =  document.getElementById('codeDropdown').innerText;
-    let code = document.getElementById('code').innerText;
+    let code = document.getElementById('code').value;
 
-    console.log(dateConditionDropDown);
+
+    console.log(dateConditionDropdown);
     console.log(dates);
-    console.log(prdconditionDropdown);
+
+    console.log(conditionDropdown);
     console.log(prdCondition);
     
     console.log(codeDropdown);
     console.log(code);
 
-
-    let data = getData(dateCondition, dateRange,condition,codeOption);
-
+    let data = getData(dateConditionDropdown,dates,conditionDropdown,prdCondition,codeDropdown,code);
     setTableData(data);
 }
 
