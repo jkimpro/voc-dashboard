49ad9c7442b48455fc1ba6f39b0d2871bbf2649e
jkimpor <jkimpro10@gmail.com>
2019-11-29 15:27:45 +0900

    javascript fixed

---
+++ public/javascripts/dropdown.js
@@ -0,0 +1,24 @@
+let dateConditionArr = ['접수일', '주문일'];
+let srConditionArr = ['SR대분류', 'SR중분류', 'SR소분류'];
+let codeArr = ['상품코드', '접수코드', '주문코드', '상품명'];
+
+
+let dropDownChange = (option, num) =>{
+    if(option ===0){
+        let origin = document.getElementById('dateConditionDropDown');
+        origin.innerText ="";
+        origin.innerText = dateConditionArr[num];
+    }
+    else if(option ===1){
+        let origin = document.getElementById('conditionDropdown');
+        origin.innerText ="";
+        origin.innerText = srConditionArr[num];
+        
+    }
+    else{
+        let origin = document.getElementById('codeDropdown');
+        origin.innerText ="";
+        origin.innerText = codeArr[num];
+   
+    }
+}
--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,3 +1,31 @@
-const colCount =0;
+let colCount =0;
 
+let getData = () =>{
+
+}
+
+let serachData = () =>{
+
+    let dateConditionDropDown = document.getElementById('dateConditionDropDown').innerText;
+    let dates =  document.getElementById('dates').innerHTML;
+    
+    let conditionDropdown = document.getElementById('conditionDropdown').innerText;
+    let prdCondition = document.getElementById('prdCondition').innerText;
+    
+    let codeDropdown =  document.getElementById('codeDropdown').innerText;
+    let code = document.getElementById('code').innerText;
+
+    console.log(dateConditionDropDown);
+    console.log(dates);
+    console.log(prdconditionDropdown);
+    console.log(prdCondition);
+    
+    console.log(codeDropdown);
+    console.log(code);
+
+
+    let data = getData(dateCondition, dateRange,condition,codeOption);
+
+    setTableData(data);
+}
 
